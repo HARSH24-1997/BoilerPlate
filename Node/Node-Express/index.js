3331f52dfb797075aea483f92db5ff1ededccbe5
HARSH24-1997 <rajput24.harsh@gmail.com>
2019-12-25 22:42:46 +0530

    without Express Router

---
+++ Node/Node-Express/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.all('/dishes',(req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    next();
+})
+
+app.get('/dishes',(req,res,next)=>{
+    res.end('will send dishes');
+});
+
+app.post('/dishes',(req,res,next)=>{
+    res.end('Will add the dish' + req.body.name + 'with details' +req.body.description); 
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+  });
+   
+  app.delete('/dishes', (req, res, next) => {
+      res.end('Deleting all dishes');
+  });
+  
+  app.get('/dishes/:dishId', (req,res,next) => {
+      res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+  });
+  
+  app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+  });
+  
+  app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+  });
+  
+  app.delete('/dishes/:dishId', (req, res, next) => {
+      res.end('Deleting dish: ' + req.params.dishId);
+  });
+
+app.use((req,res,next)=>{
+    console.log(req.headers)
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<html><body><h1>Hello, World! Express Server</h1></body></html>')
+})
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
